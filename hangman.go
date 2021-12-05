42c6d2bf463729636caa001bf82bba0b7636308b
Bahsiik <o.mist1303@gmail.com>
2021-12-05 21:53:05 +0100

    correction calcul des points

--- hangman.go
+++ hangman.go
@@ -36,24 +36,19 @@ func main() {
 	test3 := 0
 	// open the file
 	file, err := os.Open("./Ressources/words.txt")
-
 	//handle errors while opening
 	if err != nil {
 		log.Fatalf("Error when opening file: %s", err)
 	}
-
 	fileScanner := bufio.NewScanner(file)
-
 	// read line by line
 	for fileScanner.Scan() {
 		tableau = append(tableau, fileScanner.Text())
 	}
-
 	// handle first encountered error while reading
 	if err := fileScanner.Err(); err != nil {
 		log.Fatalf("Error while reading file: %s", err)
 	}
-
 	//Lecture à l'indice de la ligne
 	println(len(tableau))
 	rand.Seed(time.Now().UnixNano())
@@ -61,7 +56,6 @@ func main() {
 	println(nombre)
 	motChoisit := tableau[nombre]
 	println(motChoisit)
-
 	for i := 0; i < len(motChoisit); i++ {
 		motCache = append(motCache, "_")
 	}
@@ -75,23 +69,18 @@ func main() {
 		motCache[index] = string(motChoisit[index])
 	}
 	test3 = lettresAffichees
-
 	for i := range motCache {
 		print(motCache[i])
 	}
-
 	for tentative != 0 {
 		println()
 		println(test3, " ", len(motChoisit))
-		println()
 		println("Tentative restante :", tentative)
 		lettre := choixLettre()
 		println("la lettre choisit est : ", lettre)
-
 		test2 := 0
 		for i := 0; i < len(motChoisit); i++ {
-
-			if lettre == string(motChoisit[i]) {
+			if lettre == string(motChoisit[i]) && string(motCache[i]) == "_" {
 				motCache[i] = string(motChoisit[i])
 				test3++
 			} else {
@@ -103,12 +92,10 @@ func main() {
 		}
 		if test3 == len(motChoisit) {
 			println("\ngg Hitler")
+			break
 		}
 		for i := range motCache {
 			print(motCache[i])
 		}
-
 	}
-
-	file.Close()
 }
