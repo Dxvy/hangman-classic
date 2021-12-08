75e95949b93c336d5d921a16c79e4244b9a1449b
Bahsiik <o.mist1303@gmail.com>
2021-12-08 14:12:35 +0100

    reformatage du code

--- hangman.go
+++ hangman.go
@@ -10,7 +10,99 @@ import (
 	"time"
 )
 
-func Input() string { // Fonction pour récupérer le texte écrit dans le cmd et l'utiliser
+var array []string
+var hiddenWord []string
+var trial []string
+var attempt = 10
+var isFind = 0
+var chosenWord string
+
+func main() {
+	fileScanner := createScanner("./Ressources/words.txt")
+	array = getWords(fileScanner, array)
+	rand.Seed(time.Now().UnixNano())
+	nombre := rand.Intn(len(array))
+	chosenWord = array[nombre]
+	hiddenWord = hideToFindWord()
+	displayedLetters := showToFindLetters()
+	isFind = displayedLetters
+	start()
+}
+
+func start() {
+	for attempt != 0 {
+		verifLettersUsed := 0
+		println("Tentative restante :", attempt)
+		printToFindWord()
+		displayHangman(attempt, "./Ressources/Hangman.txt")
+		letter := trySomething()
+		for i := range trial {
+			if letter == trial[i] {
+				verifLettersUsed++
+			}
+		}
+		if verifLettersUsed != 0 {
+			println("Cette letter a déjà été faite !!!")
+			continue
+		}
+		if verifLettersUsed == 0 {
+			trial = append(trial, letter)
+		}
+		println("la letter choisit est : ", letter)
+		displayProposition(trial)
+		verifGoodProposition := 0
+		for i := 0; i < len(chosenWord); i++ {
+			if letter == string(chosenWord[i]) && string(hiddenWord[i]) == "_" {
+				hiddenWord[i] = string(chosenWord[i])
+				isFind++
+			} else {
+				verifGoodProposition++
+			}
+		}
+		if verifGoodProposition == len(chosenWord) {
+			if len(letter) == 1 {
+				attempt--
+			} else {
+				attempt -= 2
+			}
+		}
+		if isFind == len(chosenWord) {
+			println("\ngg Hitler")
+			break
+		}
+	}
+}
+
+func getWords(fileScanner *bufio.Scanner, array []string) []string {
+	for fileScanner.Scan() {
+		array = append(array, fileScanner.Text())
+	}
+	return array
+}
+
+func showToFindLetters() int {
+	lettresAffichees := (len(hiddenWord) / 2) - 1
+	for i := 0; i < lettresAffichees; i++ {
+		index := rand.Intn(len(hiddenWord))
+		hiddenWord[index] = string(chosenWord[index])
+	}
+	return lettresAffichees
+}
+
+func hideToFindWord() []string {
+	for i := 0; i < len(chosenWord); i++ {
+		hiddenWord = append(hiddenWord, "_")
+	}
+	return hiddenWord
+}
+
+func printToFindWord() {
+	for i := range hiddenWord {
+		print(hiddenWord[i])
+	}
+}
+
+func input() string { // Fonction pour récupérer le texte écrit dans le cmd et l'utiliser
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("-> ")
 	text, _ := reader.ReadString('\n')
@@ -18,161 +110,77 @@ func Input() string { // Fonction pour récupérer le texte écrit dans le cmd e
 	return text
 }
 
-func choixLettre() string {
+func trySomething() string {
 	println("Choisis une lettre (en minuscule) : ")
-	lettre := Input()
+	lettre := input()
 	return lettre
 }
 
-func main() {
-	var tableau []string
-	var motCache []string
-	tentative := 10
-	test3 := 0
-
-	// open the file
-	file, err := os.Open("./Ressources/words.txt")
-	//handle errors while opening
-	if err != nil {
-		log.Fatalf("Error when opening file: %s", err)
-	}
-	fileScanner := bufio.NewScanner(file)
-	// read line by line
-	for fileScanner.Scan() {
-		tableau = append(tableau, fileScanner.Text())
-	}
-	// handle first encountered error while reading
-	if err := fileScanner.Err(); err != nil {
-		log.Fatalf("Error while reading file: %s", err)
-	}
-	//Lecture à l'indice de la ligne
-	println(len(tableau))
-	rand.Seed(time.Now().UnixNano())
-	nombre := rand.Intn(len(tableau))
-	println(nombre)
-	motChoisit := tableau[nombre]
-	println(motChoisit)
-	var proposition []string
-	for i := 0; i < len(motChoisit); i++ {
-		motCache = append(motCache, "_")
-	}
-	for i := range motCache {
-		print(motCache[i])
+func displayProposition(proposition []string) {
+	println("Voici les propositions déjà tentées :")
+	for i := range proposition {
+		print(proposition[i] + " ")
 	}
 	println()
-	lettresAffichees := (len(motCache) / 2) - 1
-	for i := 0; i < lettresAffichees; i++ {
-		index := rand.Intn(len(motCache))
-		motCache[index] = string(motChoisit[index])
-	}
-	test3 = lettresAffichees
-	for i := range motCache {
-		print(motCache[i])
-	}
-	for tentative != 0 {
-		println()
-		affichageJose(tentative)
-		test4 := 0
-		println()
-		println(test3, " ", len(motChoisit))
-		println("Tentative restante :", tentative)
-		lettre := choixLettre()
-		for i := range proposition {
-			if lettre == proposition[i] {
-				test4++
-			}
-		}
-		if test4 != 0 {
-			println("Cette proposition a déjà été faite !!!")
-			continue
-		}
-		if test4 == 0 {
-			proposition = append(proposition, lettre)
-		}
-		for i := range proposition {
-			println(proposition[i])
-		}
-		println("la lettre choisit est : ", lettre)
-		test2 := 0
-		for i := 0; i < len(motChoisit); i++ {
-			if lettre == string(motChoisit[i]) && string(motCache[i]) == "_" {
-				motCache[i] = string(motChoisit[i])
-				test3++
-			} else {
-				test2++
-			}
-		}
-		println("longueur du choix : ", len(lettre))
-		if test2 == len(motChoisit) {
-			if len(lettre) == 1 {
-				tentative--
-			} else {
-				tentative -= 2
-			}
-		}
-		if test3 == len(motChoisit) {
-			println("\ngg Hitler")
-			break
-		}
-		for i := range motCache {
-			print(motCache[i])
-		}
-	}
 }
 
-func affichageJose(tentative int) {
+func createScanner(nomFichier string) *bufio.Scanner {
+	file, err := os.Open(nomFichier)
+	manageError(err)
+	fileScanner := bufio.NewScanner(file)
+	return fileScanner
+}
+
+func displayHangman(tentative int, nomFichier string) {
 	file, err := os.Open("./Ressources/hangman.txt")
-	if err != nil {
-		log.Fatalf("Error when opening file: %s", err)
-	}
+	manageError(err)
 	fileScanner := bufio.NewScanner(file)
 	i := 0
 	var debut int
 	var fin int
-	if tentative == 9 {
+	switch tentative {
+	case 9:
 		debut = 0
 		fin = 7
-	}
-	if tentative == 8 {
+	case 8:
 		debut = 8
 		fin = 15
-	}
-	if tentative == 7 {
+	case 7:
 		debut = 16
 		fin = 23
-	}
-	if tentative == 6 {
+	case 6:
 		debut = 24
 		fin = 31
-	}
-	if tentative == 5 {
+	case 5:
 		debut = 32
 		fin = 39
-	}
-	if tentative == 4 {
+	case 4:
 		debut = 40
 		fin = 47
-	}
-	if tentative == 3 {
+	case 3:
 		debut = 48
 		fin = 55
-	}
-	if tentative == 2 {
+	case 2:
 		debut = 56
 		fin = 63
-	}
-	if tentative == 1 {
+	case 1:
 		debut = 64
 		fin = 71
-	}
-	if tentative == 0 {
+	case 0:
 		debut = 72
 		fin = 79
 	}
+	println()
 	for fileScanner.Scan() {
 		if i >= debut && i <= fin {
 			println(fileScanner.Text())
 		}
 		i++
+	}
+}
+
+func manageError(err error) {
+	if err != nil {
+		log.Fatalf("Error when opening file: %s", err)
 	}
 }
